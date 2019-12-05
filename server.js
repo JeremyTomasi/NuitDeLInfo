e3adee58e2d1bb2af63f251e718574def2b7d53b
NecroSig <45471682+NecroSig@users.noreply.github.com>
2019-12-05 22:02:09 +0100

    erratum

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ moment.locale('fr');
 
 
 //connexion à la base de données mongodb
-mongoose.connect(connectionString,, { useUnifiedTopology: true,useNewUrlParser:true}, (err,data) => {
+mongoose.connect(connectionString, { useUnifiedTopology: true,useNewUrlParser:true}, (err,data) => {
   if (err){
     console.log("Connexion à la base de donnée : échec")
     return
