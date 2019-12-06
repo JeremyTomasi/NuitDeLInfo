21a901821c14da11914a876f02bc0e69adb698bf
NecroSig <45471682+NecroSig@users.noreply.github.com>
2019-12-06 06:14:30 +0100

    css

--- public/js/script.js
+++ public/js/script.js
@@ -3,8 +3,5 @@ var divCarte = document.getElementsByClassName("divCarteClass");
 
 for (let i = 0; i < divCarte.length; i++)
 {
-    var texte = divCarte[i].innerHTML;
-    texte[0] = " ";
-
-    divCarte[i].innerHTML = texte
+    divCarte.innerHTML = divCarte.innerText;
 }
