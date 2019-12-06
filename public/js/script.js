c46513fd1970e7e960cd49d6c3ea64addc73155f
NecroSig <45471682+NecroSig@users.noreply.github.com>
2019-12-06 06:31:02 +0100

    css

--- public/js/script.js
+++ public/js/script.js
@@ -2,4 +2,4 @@ var divCarte = document.getElementsByClassName("divCarteClass");
 var text = divCarte.innerText;
 console.log(divCarte.innerHTML);
 
-divCarte.innerHTML = $.parseHTML(divCarte[0].innerText);
+document.write(divCarte[0].innerHTML);
